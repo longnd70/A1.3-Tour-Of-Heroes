6e95b0cb721a2f17d6caa75cc23e4b6efdf10552
longnd70 <longnd70@gmail.com>
2023-01-26 21:42:45 +0700

    1st Update
    First update

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[]=[];
+  selectedHero?: Hero;
+  constructor(private heroService:HeroService, private messageService: MessageService) { }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(Heroes=>this.heroes=Heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+}
